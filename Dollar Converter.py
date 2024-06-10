ddc45ac073c5c7e83a1e5fafbf65b0c22f9ee0c1
danielfilemon <107655438+danielfilemon@users.noreply.github.com>
2024-06-10 16:49:25 -0300

    Update Dollar Converter.py

--- Dollar Converter.py
+++ Dollar Converter.py
@@ -1,6 +1,7 @@
-real = float(input('How much money do you have in your wallet?'))
-dolar = real / 5.00
-real= euro / 10.00
-print('I have R$ {:.2f} Buy US$ {:.2f}'.format(real, dolar))
-print('I have R$ {:.2f} Buy US€ {:.2f}'.format(real, euro))
+real = float(input('How much money do you have in your wallet? R$ '))
+dollar = real / 5.00  # Considering the current exchange rate of 1 USD = 5 BRL
+euro = real / 6.00  # Considering the current exchange rate of 1 EUR = 6 BRL
+
+print('I have R$ {:.2f}. With this amount, you can buy approximately US$ {:.2f}.'.format(real, dollar))
+print('I have R$ {:.2f}. With this amount, you can buy approximately € {:.2f}.'.format(real, euro))
 
